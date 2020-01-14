5cbdcca6c7fa180fe94d4ffbcd14ee0f6b5b3690
Closure_Bot <@>
2020-01-15 00:51:48 +0800

    [KR UPDATE] Client:0.8.13 Data:20-01-14-09-48-15-67d362

--- ko_KR/gamedata/[uc]lua/hotfixes/definedfix.lua
+++ ko_KR/gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -3,7 +3,10 @@ local list =
 {
   --list all defined fixer path at here
   "HotFixes/ShopHotfixer",
-  "HotFixes/Act4d0Hotfixer"
+  "HotFixes/ShopFurnHotfixer",
+  "HotFixes/Act4d0Hotfixer",
+  "HotFixes/InventoryHotfixer",
+  "HotFixes/YostarSDKFixer"
 };
 
 return list;
