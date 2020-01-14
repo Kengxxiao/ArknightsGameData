929099a62b34912c02afdb3abd8f0461aa016aec
Closure_Bot <@>
2020-01-15 00:39:08 +0800

    [EN UPDATE] Client:0.8.13 Data:20-01-09-15-22-02-151145

--- en_US/gamedata/[uc]lua/hotfixes/definedfix.lua
+++ en_US/gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -3,8 +3,7 @@ local list =
 {
   --list all defined fixer path at here
   "HotFixes/ShopHotfixer",
-  "HotFixes/Act4d0Hotfixer",
-  "HotFixes/InventoryHotfixer"
+  "HotFixes/Act4d0Hotfixer"
 };
 
 return list;
