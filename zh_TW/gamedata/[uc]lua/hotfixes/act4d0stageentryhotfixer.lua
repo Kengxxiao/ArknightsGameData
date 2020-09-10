af5e6c59ff5285ccb5b92c4f5d14219b9050a3d8
可露希尔_BOTv5 <@>
2020-09-10 13:31:23 +0800

    [TC UPDATE] Client:0.8.14 Data:20-09-08-06-33-28-b7783d

--- zh_TW/gamedata/[uc]lua/hotfixes/act4d0stageentryhotfixer.lua
+++ zh_TW/gamedata/[uc]lua/hotfixes/act4d0stageentryhotfixer.lua
@@ -17,6 +17,23 @@ function Act4d0StageEntryHotfixer:OnInit()
   end)
 end
 
+function Act4d0StageEntryHotfixer.Split(szFullString, szSeparator)
+local nFindStartIndex = 1
+local nSplitIndex = 1
+local nSplitArray = {}
+while true do
+   local nFindLastIndex = string.find(szFullString, szSeparator, nFindStartIndex)
+   if not nFindLastIndex then
+    nSplitArray[nSplitIndex] = string.sub(szFullString, nFindStartIndex, string.len(szFullString))
+    break
+   end
+   nSplitArray[nSplitIndex] = string.sub(szFullString, nFindStartIndex, nFindLastIndex - 1)
+   nFindStartIndex = nFindLastIndex + string.len(szSeparator)
+   nSplitIndex = nSplitIndex + 1
+end
+return nSplitArray
+end
+
 function Act4d0StageEntryHotfixer.FixTimeInfoDisplay(self)
   local data = CS.Torappu.Activity.Act4D0.Act4D0ResUtil.basicData
   local endTime = data.endTime
@@ -25,9 +42,10 @@ function Act4d0StageEntryHotfixer.FixTimeInfoDisplay(self)
     local find_func = CS.Torappu.Lua.LuaUIUtil.GetChild
     local target = find_func(self.gameObject, "remain_time")
     target = target:GetComponent("Text")
-    local splited_time = string.split(target.text, ' ')
-    if splited_time.Length > 2 then
-      splited_time[2] = "09/10/16:00 - 09/24/03:59"
+    local splited_time = Act4d0StageEntryHotfixer.Split(target.text," ")
+    if #splited_time > 4 then
+      --eutil.LogError('hereeeee')
+      splited_time[4] = "09/10/16:00 - 09/24/03:59"
       target.text = table.concat(splited_time, ' ')
     end
   end
