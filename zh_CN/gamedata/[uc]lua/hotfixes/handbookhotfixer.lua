2dc609fa2c39ab6dbb6782e454fe8838ee50c36c
可露希尔_BOTv9 <@>
2021-09-24 16:10:30 +0800

    [CN UPDATE] Client:1.5.81 Data:21-09-22-14-28-33-3b3842

--- zh_CN/gamedata/[uc]lua/hotfixes/handbookhotfixer.lua
+++ zh_CN/gamedata/[uc]lua/hotfixes/handbookhotfixer.lua
@@ -13,8 +13,13 @@ local function _FixNewVoiceTrackPoint(self, charId)
   self:UpdateState(charId)
 end
 
+local function _GetVoicePathByType(self, wordData, preferLang, param3)
+  return self:GetVoicePathByType(wordData, preferLang, true)
+end
+
 function HandbookHotfixer:OnInit()
   self:Fix_ex(CS.Torappu.UI.HandBook.HandBookNewVoiceTrackPointModel, "UpdateState", _FixNewVoiceTrackPoint)
+  self:Fix_ex(CS.Torappu.CharWord.VoiceLangManager, "GetVoicePathByType", _GetVoicePathByType)
 end
 
 function HandbookHotfixer:OnDispose()
