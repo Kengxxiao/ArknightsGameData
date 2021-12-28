6b3a3af176d9ac30624813bf4cd846f7663daa01
可露希尔_BOTv9 <@>
2021-12-28 16:11:23 +0800

    [CN UPDATE] Client:1.7.01 Data:21-12-25-11-18-44-23a573

--- zh_CN/gamedata/[uc]lua/hotfixes/uimischotfixer.lua
+++ zh_CN/gamedata/[uc]lua/hotfixes/uimischotfixer.lua
@@ -4,6 +4,7 @@
 
 
 local UIMiscHotfixer = Class("UIMiscHotfixer", HotfixBase)
+local eutil = CS.Torappu.Lua.Util
 
 local function _InvokeOpenPage(pageName)
   CS.Torappu.UI.UIPageController.OpenPage(pageName)
@@ -18,9 +19,61 @@ local function _LoadInitPage(self)
   self:_LoadInitPage()
 end
 
+local function _FixMissionRewardTask(self, dataWrapper)
+  self:AsyncSetData(dataWrapper)
+  local data = dataWrapper.data
+  if data == nil then
+    return
+  end
+  local rewardCount = 0
+  if data.rewards ~= nil then
+    rewardCount = data.rewards.Count
+  end
+  eutil.SetActiveIfNecessary(self._item1.gameObject, rewardCount >= 1)
+  eutil.SetActiveIfNecessary(self._item2.gameObject, rewardCount >= 2)
+end
+
+local function _FixBuildingRoomLevel(self, roomModel, isSelected)
+  local prevRoomType = self.m_cachedRoomType
+  self:Render(roomModel, isSelected)
+  if roomModel == nil then
+    return 
+  end
+  local curType = roomModel.roomId
+  if prevRoomType == curType then
+    return
+  end
+  local isDorm = curType == CS.Torappu.BuildingData.RoomType.DORMITORY
+  if isDorm then
+    return
+  end
+  local adapter = self._commonLevelView.m_adapter
+  if adapter == nil then
+    return
+  end
+  adapter:NotifyDataSetChanged()
+end
+
 function UIMiscHotfixer:OnInit()
   xlua.private_accessible(CS.Torappu.UI.UIPageController)
   self:Fix_ex(CS.Torappu.UI.UIPageController, "_LoadInitPage", _LoadInitPage)
+
+  xlua.private_accessible(CS.Torappu.UI.Mission.DailyMissionRewardTask)
+  self:Fix_ex(CS.Torappu.UI.Mission.DailyMissionRewardTask, "AsyncSetData", function(self, dataWrapper)
+    local ok, ret = xpcall(_FixMissionRewardTask, debug.traceback, self, dataWrapper)
+    if not ok then
+      CS.UnityEngine.DLog.LogError(ret)
+    end
+  end);
+
+  xlua.private_accessible(CS.Torappu.Building.UI.SM.BuildingSMRoomItemView)
+  xlua.private_accessible(CS.Torappu.Building.UI.BuildingRoomLevelView)
+  self:Fix_ex(CS.Torappu.Building.UI.SM.BuildingSMRoomItemView, "Render", function(self, roomModel, isSelected)
+    local ok, ret = xpcall(_FixBuildingRoomLevel, debug.traceback, self, roomModel, isSelected)
+    if not ok then
+      CS.UnityEngine.DLog.LogError(ret)
+    end
+  end);
 end
 
 function UIMiscHotfixer:OnDispose()
