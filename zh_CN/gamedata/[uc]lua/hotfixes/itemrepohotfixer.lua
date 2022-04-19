f636b57f134f9d11c3fa96d9c0df65bf6ccf239f
可露希尔_BOTv10 <@>
2022-04-20 01:10:31 +0800

    [CN UPDATE] Client:1.8.01 Data:22-04-19-14-08-43-c0b3a6

--- zh_CN/gamedata/[uc]lua/hotfixes/itemrepohotfixer.lua
+++ zh_CN/gamedata/[uc]lua/hotfixes/itemrepohotfixer.lua
@@ -4,15 +4,17 @@ local function _UpdateStageDropInfoFix(self,viewModel,descViewModel)
 	local stageDataMainLine = CS.Torappu.StageDataUtil:GetMainStageProgress();	
 	if (stageDataMainLine == nil)then
 		local curTs = CS.Torappu.DateTimeUtil.timeStampNow;
-		for i = viewModel.stageDrop.Count - 1,0,-1 do
-			local dropInfo = viewModel.stageDrop[i]
-			local stageSucc,stageDataWrapper = CS.Torappu.StageDataUtil.TryGetStageWrapper(dropInfo.stageId, curTs)
-			if (stageSucc) then
-	          	local stageData = stageDataWrapper.data;
-	          	if (stageData ~= nil) and (stageData.stageType == CS.Torappu.StageType.MAIN) then
-					local succ, playerStage = CS.Torappu.PlayerData.instance.data.dungeon.stages:TryGetValue(dropInfo.stageId)
-					if (succ == false) then
-						viewModel.stageDrop:RemoveAt(i)
+		if (viewModel.stageDrop ~= nil) then
+			for i = viewModel.stageDrop.Count - 1,0,-1 do
+				local dropInfo = viewModel.stageDrop[i]
+				local stageSucc,stageDataWrapper = CS.Torappu.StageDataUtil.TryGetStageWrapper(dropInfo.stageId, curTs)
+				if (stageSucc) then
+		          	local stageData = stageDataWrapper.data;
+		          	if (stageData ~= nil) and (stageData.stageType == CS.Torappu.StageType.MAIN) then
+						local succ, playerStage = CS.Torappu.PlayerData.instance.data.dungeon.stages:TryGetValue(dropInfo.stageId)
+						if (succ == false) then
+							viewModel.stageDrop:RemoveAt(i)
+						end
 					end
 				end
 			end
