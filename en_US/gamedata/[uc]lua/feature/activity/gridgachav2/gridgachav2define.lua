c9c389ef890c8421969b6fb7a09858a27a96d663
可露希尔_BOTv10 <@>
2022-04-28 19:11:58 +0800

    [EN UPDATE] Client:9.0.01 Data:22-04-21-07-08-02-5efcb8

---
+++ en_US/gamedata/[uc]lua/feature/activity/gridgachav2/gridgachav2define.lua
@@ -0,0 +1,26 @@
+GridGachaV2ServerCode = {
+  GACHA = "/activity/gridGachaV2/doTodayGacha"
+}
+
+GridGachaV2AnimationName = {
+  BANNER = "banner",
+  CRITICAL = "item_critical",
+  NORMAL = "item_normal",
+  GRAND = "item_grand",
+  SCAN_LINE = "scan_line",
+  RED_SCAN_LINE = "red_scan_line",
+  RIGHT_PANEL_1 = "right_panel_part_one",
+  RIGHT_PANEL_2 = "right_panel_part_two",
+  BUTTON = "start_button_anim",
+  RED_CIRCLE = "item_red_circle",
+  SEGMENT = "item_segment"
+}
+
+GridGachaV2SoundName = {
+  BLINK = "ON_GRIDGACHA_BLINK",
+  LASER = "ON_GRIDGACHA_LASER",
+  LOCATE = "ON_GRIDGACHA_LOCATE",
+  SCAN = "ON_GRIDGACHA_SCAN",
+  TICK = "ON_NUMBER_TICK",
+  BANNER = "ON_MSGBOX_POPUP"
+}
