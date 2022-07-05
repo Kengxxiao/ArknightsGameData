6a4b7e52d3af71e9df3269a17884385cc95f1014
可露希尔_BOTv10 <@>
2022-07-05 10:10:48 +0800

    [CN UPDATE] Client:1.8.41 Data:22-07-03-10-18-57-3d89c9

--- zh_CN/gamedata/[uc]lua/globalconfig.lua
+++ zh_CN/gamedata/[uc]lua/globalconfig.lua
@@ -1,6 +1,6 @@
 GlobalConfig = 
 {
-  CUR_FUNC_VER = "V028",
+  CUR_FUNC_VER = "V029",
 }
 
 
--- zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
+++ zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -3,13 +3,6 @@ local list =
 {
   
   "HotFixes/ShopHotfixer",
-  "HotFixes/ScatteredProjectileHitBehaviourHotfixer",
-  "HotFixes/ClimbTowerLayerViewValueChangedHotfixer",
-  "HotFixes/RoguelikeMonthSquadViewModelHotfixer",
-  "HotFixes/UIItemDescFloatStageDropDetailHotfixer",
-  "HotFixes/UniEquipSingleEquipViewHotfixer",
-  "HotFixes/LegionUILibraryCardItemHotfixer",
-  "HotFixes/RoguelikeCharEquipHotfixer",
 };
 
 return list;
