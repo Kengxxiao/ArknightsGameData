1259603a38d7e4c642eba876fe828df03486ebfc
可露希尔_BOTv10 <@>
2022-07-26 23:10:36 +0800

    [CN UPDATE] Client:1.8.41 Data:22-07-26-12-01-49-1c8fc3

--- zh_CN/gamedata/[uc]lua/hotfixes/uihotfixer.lua
+++ zh_CN/gamedata/[uc]lua/hotfixes/uihotfixer.lua
@@ -1,26 +1,23 @@
-
-
-
+local eutil = CS.Torappu.Lua.Util
 
 
 local UIHotfixer = Class("UIHotfixer", HotfixBase)
 
-local function SendDiamondExchangeService(self)
-  local payCheckView = CS.Torappu.UI.UIPayCostCheckView.GetActiveInst()
-  if payCheckView == nil then
-    return
+local function _FixRefreshCharmsList(self) 
+  self:_RefreshCharmsList()
+  if self.m_charmList == nil then
+    local StringList = CS.System.Collections.Generic.List(CS.System.String)
+    self.m_charmList = StringList()
   end
-  if payCheckView:CheckNeedBuyDiamond(self.m_cacheDiamond) then
-    self:Dismiss()
-    return
-  end
-
-  self:SendDiamondExchangeService()
 end
 
 function UIHotfixer:OnInit()
-  xlua.private_accessible(CS.Torappu.UI.Recruit.RecruitBuyDiamondShardView)
-	self:Fix_ex(CS.Torappu.UI.Recruit.RecruitBuyDiamondShardView,"SendDiamondExchangeService", SendDiamondExchangeService)
+  self:Fix_ex(CS.Torappu.UI.Squad.SquadHomeStateBean, "_RefreshCharmsList", function(self)
+    local ok, errorInfo = xpcall(_FixRefreshCharmsList, debug.traceback, self)
+    if not ok then
+      eutil.LogError("[_RefreshCharmsList] error " .. errorInfo)
+    end
+  end)
 end
 
 function UIHotfixer:OnDispose()
