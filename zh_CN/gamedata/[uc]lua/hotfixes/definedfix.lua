55a657bae8bde771f678090ee85d249111b1ef0a
可露希尔_BOTv10 <@>
2022-08-31 16:11:01 +0800

    [CN UPDATE] Client:1.8.61 Data:22-08-29-13-56-31-95a9a4

--- zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
+++ zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -5,7 +5,8 @@ local list =
   "HotFixes/ShopHotfixer",
   "HotFixes/BossRushHotfixer",
   "HotFixes/HpRatioToggleCheckerHotfixer",
-  "HotFixes/BossRushSquadHomeStateHotfixer"
+  "HotFixes/BossRushSquadHomeStateHotfixer",
+  "HotFixes/UIEquipSelectFix"
 };
 
 return list;
