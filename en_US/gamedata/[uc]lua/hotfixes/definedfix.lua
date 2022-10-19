cafebd32d96cddf78b78442c832f4d841356a9dd
MizukiBot_v1 <@>
2022-10-19 19:12:11 +0800

    [EN UPDATE] Client:12.0.01 Data:22-10-10-05-55-30-b5f528

--- en_US/gamedata/[uc]lua/hotfixes/definedfix.lua
+++ en_US/gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -3,9 +3,14 @@ local list =
 {
   
   "HotFixes/ShopHotfixer",
+  "HotFixes/DynamicMusicCompInfoHotfixer",
+  "HotFixes/ItemRepoHotfixer",
+  "HotFixes/InParamsHotfixer",
   "HotFixes/UIHotfixer",
-  "HotFixes/CharacterRepoStateBeanHotFixer",
-  "HotFixes/RoguelikeFriendAssistDetailStateHotFixer",
+  "HotFixes/NameCardHotfixer",
+  "HotFixes/StagePreviewStateHotfixer",
+  "HotFixes/DeepSeaRPBattlePreviewStateHotfixer",
+  "HotFixes/DeepSeaRolePlayPageHotfixer",
 };
 
 return list;
