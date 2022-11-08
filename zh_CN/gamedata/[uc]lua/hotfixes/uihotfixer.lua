9a9de82397b33a1d2eba1a8805e005593a3dd0f8
MizukiBot_v1 <@>
2022-11-08 16:01:25 +0800

    [CN UPDATE] Client:1.9.21 Data:22-11-04-16-30-11-3b94fc

--- zh_CN/gamedata/[uc]lua/hotfixes/uihotfixer.lua
+++ zh_CN/gamedata/[uc]lua/hotfixes/uihotfixer.lua
@@ -3,6 +3,17 @@
 
 
 local UIHotfixer = Class("UIHotfixer", HotfixBase)
+local eutil = CS.Torappu.Lua.Util
+local SiracusaMapController = CS.Torappu.UI.SiracusaMap.SiracusaMapController
+
+local function OnNavigationSelectFix(self, entryId, entryType)
+  local mapViewModel = self.panelMapProperty:GetValueNotNull()
+  if mapViewModel.navigationViewModel.selectingEntryId ~= entryId then
+    mapViewModel:SelectPoint(nil)
+  end
+  mapViewModel:SelectNavi(entryId)
+  self.panelMapProperty:NotifyUpdate()
+end
 
 function UIHotfixer:OnInit()
   self:Fix("Torappu.UI.SiracusaMap.SiracusaMapBigMapView+ScrollController", "set_normalizedPos", function(self, val)
@@ -11,6 +22,22 @@ function UIHotfixer:OnInit()
       return
     end
     scrollRect.normalizedPosition = val
+  end)
+
+  xlua.private_accessible(SiracusaMapController)
+  self:Fix_ex(SiracusaMapController, "_OnNavigationSelect", function(self, entryId, entryType)
+    local ok, ret = xpcall(OnNavigationSelectFix, debug.traceback, self, entryId, entryType)
+    if not ok then
+      eutil.LogError("[OnNavigationSelectFix] fix" .. ret)
+    end
+  end)
+
+  self:Fix(CS.Torappu.ResourceUrls, "GetDeepSeaRPSpecialPicHubPath", function()
+    return "Arts/UI/DeepSea/SpecialPic/special_pic_hub"
+  end)
+
+  self:Fix(CS.Torappu.ResourceUrls, "GetDeepSeaRPPicHubPath", function()
+    return "Arts/UI/DeepSea/NormalPic/normal_pic_hub"
   end)
 end
 
