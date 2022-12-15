3052d88c1a5c46b2c09d155c35947de055c89390
MizukiBot_v1 <@>
2022-12-15 10:21:07 +0800

    [CN UPDATE] Client:1.9.42 Data:22-12-14-13-42-39-ee18cc

--- zh_CN/gamedata/[uc]lua/globalconfig.lua
+++ zh_CN/gamedata/[uc]lua/globalconfig.lua
@@ -1,6 +1,6 @@
 GlobalConfig = 
 {
-  CUR_FUNC_VER = "V033",
+  CUR_FUNC_VER = "V034",
 }
 
 
--- zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
+++ zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -3,7 +3,6 @@ local list =
 {
   
   "HotFixes/ShopHotfixer",
-  "HotFixes/UIHotfixer",
 };
 
 return list;
