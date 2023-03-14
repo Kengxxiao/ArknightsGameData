f204a9bd931188bd05c7fe0483ea5ea730c9ba86
MizukiBot_v1 <@>
2023-03-14 16:10:55 +0800

    [CN UPDATE] Client:1.9.81 Data:23-03-10-19-53-58-216e5f

--- zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
+++ zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -3,6 +3,10 @@ local list =
 {
   
   "HotFixes/ShopHotfixer",
+  "HotFixes/HandbookHotfixer",
+  "HotFixes/TileHotfixer",
+  "HotFixes/MhwrctSkill_1Hotfixer",
+  "HotFixes/MhwrbgSkill_1Hotfixer"
 };
 
 return list;
