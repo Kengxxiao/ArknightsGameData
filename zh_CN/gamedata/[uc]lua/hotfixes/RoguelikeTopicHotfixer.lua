b9f6dc91263a48c1327c21340dcc7dda64c5b676
MuelsyseBot_v1 <@>
2024-07-25 16:02:44 +0800

    [CN UPDATE] Client:2.3.01 Data:24-07-23-15-16-02-a53606

--- zh_CN/gamedata/[uc]lua/hotfixes/RoguelikeTopicHotfixer.lua
+++ zh_CN/gamedata/[uc]lua/hotfixes/RoguelikeTopicHotfixer.lua
@@ -38,10 +38,49 @@ local function _RenderInitSquadWithDifficultyFix(self, viewModel)
       break;
     end
 
-    self._bandRankAtlasObject = atlasObject;
+    self._difficultyAtlasObject = atlasObject;
   until(true);
 
   self:_RenderInitSquadWithDifficulty(viewModel);
+end
+
+local function _TryRemoveBindingTiles(self, tiles)
+  if tiles == nil then
+    return false
+  end
+  local hasNew = false
+  local tilesCount = tiles.Count
+  local luaCreateCnt = CS.Torappu.Battle.RemovableSharedRandomTileGlobalBuff.createCnt
+  local luaS_targetTiles = CS.Torappu.Battle.RemovableSharedRandomTileGlobalBuff.s_targetTiles
+  local luaEffectHolder = CS.Torappu.Battle.RemovableSharedRandomTileGlobalBuff.effectHolder
+  for i = 0, tilesCount - 1 do
+    local tile = tiles[i]
+    if tile ~= nil then
+      if self:FilterTile(tile) then
+        flag, cnt = luaCreateCnt:TryGetValue(tile.gridPosition)
+        if flag and cnt > 0 then
+          luaCreateCnt[tile.gridPosition] = cnt - 1
+          if cnt - 1 <= 0 then
+            luaS_targetTiles:Remove(tile.gridPosition)
+            self.targetTiles:Remove(tile.gridPosition)
+            local character = tile:GetCharacter()
+            if character ~= nil then
+              self:TryRemoveBuff(character, false)
+            end
+            if not string.isNullOrEmpty(self.tileEffect) then
+              hasEffect, curTileEffect = luaEffectHolder:TryGetValue(tile)
+              if hasEffect and curTileEffect ~= nil then
+                curTileEffect:FinishMe(true)
+                luaEffectHolder:Remove(tile)
+              end
+            end
+            hasNew = true
+          end
+        end
+      end
+    end
+  end
+  return hasNew
 end
 
 function RoguelikeTopicHotfixer:OnInit()
@@ -52,6 +91,13 @@ function RoguelikeTopicHotfixer:OnInit()
       LogError("[RoguelikeTopicHotfixer] fix _RenderInitSquadWithDifficulty error: " .. errorInfo)
     end
   end)
+  xlua.private_accessible(CS.Torappu.Battle.RemovableSharedRandomTileGlobalBuff)
+  self:Fix_ex(CS.Torappu.Battle.RemovableSharedRandomTileGlobalBuff, "TryRemoveBindingTiles", function(self, tiles)
+    local ok, errorInfo = xpcall(_TryRemoveBindingTiles, debug.traceback, self, tiles)
+    if not ok then
+      LogError("[RemovableSharedRandomTileGlobalBuffHotfixer] fix" .. errorInfo)
+    end
+  end)     
 end
 
 function RoguelikeTopicHotfixer:OnDispose()
