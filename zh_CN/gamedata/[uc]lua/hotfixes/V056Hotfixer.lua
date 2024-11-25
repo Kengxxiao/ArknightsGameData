9f2ebfc908b66e87bf3af2adaa80a4966e0d54d2
AngelinaBot_v2 <@>
2024-11-25 16:00:24 +0800

    [CN UPDATE] Client:2.4.01 Data:24-11-21-11-04-45-bae23b

--- zh_CN/gamedata/[uc]lua/hotfixes/V056Hotfixer.lua
+++ zh_CN/gamedata/[uc]lua/hotfixes/V056Hotfixer.lua
@@ -146,6 +146,20 @@ local function Fix_FireworkCraftModel_LoadData(self, input)
   self:LoadData(input)
 end
 
+local function Fix_Act1VAutoChessShopQuickAssist_TryRefreshNormalItemView(self, arg1)
+  if self.view == nil then
+    return;
+  end
+  self.view:Render(arg1);
+end
+
+local function Fix_Act1VAutoChessShopQuickAssist_TryRefreshTitleItemView(self, arg1)
+  if self.view == nil then
+    return;
+  end
+  self.view:Render(arg1);
+end
+
 function V056Hotfixer:OnInit()
   xlua.private_accessible(ClsHotUpdater);
   self:Fix_ex(ClsHotUpdater, "_CalcUpdateResParams", function(newUpdateInfo, persistentResInfo, downloadPart)
@@ -194,6 +208,20 @@ function V056Hotfixer:OnInit()
       LogError("fix FireworkCraftModel error:" .. value)
       self:LoadData(input)
     end
+  end)
+  xlua.private_accessible(CS.Torappu.Activity.Act1VAutoChess.Act1VAutoChessChessShopQuickAssistOnlyItemView.VirtualView)
+  self:Fix_ex(CS.Torappu.Activity.Act1VAutoChess.Act1VAutoChessChessShopQuickAssistOnlyItemView.VirtualView, "TryRefreshAssistItemView", function(self, arg1)
+    local ok, errorInfo = xpcall(Fix_Act1VAutoChessShopQuickAssist_TryRefreshNormalItemView, debug.traceback, self, arg1)
+      if not ok then
+         LogError("fix Act1VAutoChessChessShopQuickAssistOnlyItemView virtualView error" .. errorInfo)
+      end
+  end)
+  xlua.private_accessible(CS.Torappu.Activity.Act1VAutoChess.Act1VAutoChessChessShopQuickAssistTitleWithItemView.VirtualView)
+  self:Fix_ex(CS.Torappu.Activity.Act1VAutoChess.Act1VAutoChessChessShopQuickAssistTitleWithItemView.VirtualView, "TryRefreshAssistItemView", function(self, arg1)
+    local ok, errorInfo = xpcall(Fix_Act1VAutoChessShopQuickAssist_TryRefreshTitleItemView, debug.traceback, self, arg1)
+      if not ok then
+         LogError("fix Act1VAutoChessChessShopQuickAssistTitleWithItemView virtualView error" .. errorInfo)
+      end
   end)
 end
 
