481431a1f0be574f436db4b2e1dbe9e374d0cc74
AngelinaBot_v2 <@>
2025-02-15 04:30:15 +0800

    [CN UPDATE] Client:2.4.41 Data:25-02-14-16-11-02-89657b

--- zh_CN/gamedata/[uc]lua/hotfixes/EtlchiHotfixer.lua
+++ zh_CN/gamedata/[uc]lua/hotfixes/EtlchiHotfixer.lua
@@ -7,12 +7,12 @@ local function _GetNearestEndPointTile(self, sourcePos, motionMode, avoidCheckRe
   return self:GetNearestEndPointTile(sourcePos, motionMode, avoidCheckReachable)
 end
 
-local function _EnsureDmgOrHealExecute(self, blackboard, sourceType, snapshot)
+local function _FilterLevelId(self, blackboard, sourceType, snapshot)
   local snapshotModifier = snapshot.modifier
   local snapshotSource = snapshot.source
   local localSnapshot = snapshot
   local isEntlec = blackboard:GetFloatOrDefault("is_entlec", 0)
-  if not snapshotModifier.isCancelled and snapshotModifier.damageType == CS.Torappu.Battle.DamageType.PURE and isEntlec > 0.5 then
+  if not snapshotModifier.isCancelled and snapshotModifier.damageType == CS.Torappu.Battle.DamageType.PURE and isEntlec > 0.5 and snapshotModifier:CheckSharedFlag(CS.Torappu.Battle.Modifier.SharedFlagIndex.DAMAGE_WITHOUT_MODIFY) and snapshotModifier.source == nil then
     snapshotModifier.value = blackboard:GetFpOrDefault("value", snapshotModifier.value)
     localSnapshot.modifier = snapshotModifier
     snapshot = localSnapshot
@@ -27,11 +27,11 @@ function EtlchiHotfixer:OnInit()
     local ok, endPos, route = xpcall(_GetNearestEndPointTile, debug.traceback, self, sourcePos, motionMode, avoidCheckReachable)
     return endPos, route
   end)
-  xlua.private_accessible(CS.Torappu.Battle.Action.Nodes.EnsureDmgOrHeal)
-  self:Fix_ex(CS.Torappu.Battle.Action.Nodes.EnsureDmgOrHeal, "Execute", function(self, blackboard, sourceType, snapshot)
-    local ok, res, snapshot = xpcall(_EnsureDmgOrHealExecute, debug.traceback, self, blackboard, sourceType, snapshot)
+  xlua.private_accessible(CS.Torappu.Battle.Action.Nodes.FilterLevelId)
+  self:Fix_ex(CS.Torappu.Battle.Action.Nodes.FilterLevelId, "Execute", function(self, blackboard, sourceType, snapshot)
+    local ok, res, snapshot = xpcall(_FilterLevelId, debug.traceback, self, blackboard, sourceType, snapshot)
     if not ok then
-      LogError("[_EnsureDmgOrHealExecute] fix" .. res)
+      LogError("[_FilterLevelId] fix" .. res)
     end
     return res, snapshot
   end)
