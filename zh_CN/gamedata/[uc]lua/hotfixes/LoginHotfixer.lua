ee8ae42aa427fa5c185270804b0a7dd530f7a046
Vulpisfoglia_BOT_rev20250407 <@>
2025-05-08 16:00:29 +0800

    [CN UPDATE] Client:2.5.60 Data:25-05-07-08-13-14_2c35c4

--- zh_CN/gamedata/[uc]lua/hotfixes/LoginHotfixer.lua
+++ zh_CN/gamedata/[uc]lua/hotfixes/LoginHotfixer.lua
@@ -27,12 +27,6 @@ local function SetMoviePath(self, path)
   local status = self:GetCurrentStatus()
   if CS.Torappu.GameFlowController.currentScene == "hot_update" then
     local isTargetMovie = string.find(path, "MT01")  
-    if isTargetMovie then
-      local size = self._rect.sizeDelta;
-      size.x = 1560
-      size.y = 720
-      self._rect.sizeDelta = size
-    end
     if status == CS.Torappu.Video.AbstractMediaPlayerHolder.Status.PLAYEND and isTargetMovie then
       self:Play()
       return
@@ -42,6 +36,22 @@ local function SetMoviePath(self, path)
     return
   end
   self:SetPath(path)
+end
+
+local function Play(self)
+  
+  if CS.Torappu.GameFlowController.currentScene == "hot_update" then
+    self._moviePlayer:Play()
+
+    local Vector2Int = CS.UnityEngine.Vector2Int
+
+    local movieSize = Vector2Int(1560,720)
+    local holdSize = Vector2Int(self._holder.rect.width, self._holder.rect.height)
+
+    local movieSize = CS.Torappu.GraphicUtil.Scale2DByMaxValue(movieSize, holdSize)
+    self._rect.sizeDelta = CS.UnityEngine.Vector2(movieSize.x, movieSize.y)
+  end
+  self:Play()
 end
 
 local function OnApplicationPause(self ,status)
@@ -62,6 +72,13 @@ function LoginHotfixer:OnInit()
     if not ok then
       self:SetPath(path)
       LogError("Failed to SetMoviePath : " .. err)
+    end
+  end)
+    self:Fix_ex(CS.Torappu.Video.SofdecMediaPlayer, "Play", function(self)
+    local ok, err = xpcall(Play, debug.traceback, self)
+    if not ok then
+      self:Play(path)
+      LogError("Failed to Play : " .. err)
     end
   end)
   xlua.private_accessible(typeof(CS.Torappu.UI.HotUpdate.HotUpdaterPreMainMediaView))
